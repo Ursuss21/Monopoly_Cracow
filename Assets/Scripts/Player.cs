74a85f6f2e0feaff60b48da8a57ca096c5d41d7b
Ursuss21 <robert.skrzypczak21@gmail.com>
2020-02-10 17:05:25 +0100

    Added textures and materials to property fields.
    Property and Player classes created with the basic statistics.
    Field costs added.
    Created basic pawn.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int money;
+    private List<Property> properties;
+
+    private void Start() {
+        money = 3000;
+        properties = new List<Property>();
+    }
+
+    void UpdateMoney(int value){
+        money += value;
+    }
+
+}
---
+++ Assets/Scripts/Property.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour
+{
+    [SerializeField]
+    int cost;
+    [SerializeField]
+    int mortgageCost;
+    [SerializeField]
+    bool isRail;
+    [SerializeField]
+    bool isSupplies;
+
+    [SerializeField]
+    int homeCost;
+    [SerializeField]
+    int hotelCost;
+
+    [SerializeField]
+    int[] fees = new int[6];
+    [SerializeField]
+    int set;
+
+    [SerializeField]
+    string name;
+
+    int owner;
+
+    private void Start() {
+        
+    }
+}
